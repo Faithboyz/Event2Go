aa887d4fdeaf84e3ac5c460773409027a146eb0d
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-02-04 17:58:12 -0500

    v04

--- www/app.js
+++ www/app.js
@@ -3,13 +3,13 @@
 // Declare app level module which depends on views, and components
 angular.module('e2g', [
   'ngRoute',
-  'e2g.login',
-  'e2g.signup',
   'e2g.home',
+  'e2g.signup',
+  'e2g.welcome',
   'e2g.event',
   'e2g.account'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 
-  $routeProvider.otherwise({redirectTo: '/login'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
