70ff4c29e12c5737e143ae41ea34906bb7da7130
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-02-23 19:32:12 -0500

    pass v0.2

---
+++ www/account/account.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('Event2Go.welcome', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/welcome',{
+		templateUrl: 'welcome/welcome.html',
+		controller: 'WelcomeCtrl'
+	});
+}])
+.controller('WelcomeCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', '$location', function($scope, CommonProp, $firebaseArray, $firebaseObject, $location){
+	$scope.username = CommonProp.getUser();
+
+	if(!$scope.username){
+		$location.path('/home');
+	}
+
+	var ref = firebase.database().ref().child('Articles');
+	$scope.articles = $firebaseArray(ref);	
+
+	$scope.editEvent = function(id){
+		var ref = firebase.database().ref().child('Articles/' + id);
+		$scope.editPostData = $firebaseObject(ref);
+	};
+
+	$scope.updateEvent = function(id){
+		var ref = firebase.database().ref().child('Articles/' + id);
+		ref.update({
+			title: $scope.editPostData.title,
+			post: $scope.editPostData.post
+		}).then(function(ref){
+			$scope.$apply(function(){
+				$("#editModal").modal('hide');
+			});
+		}, function(error){
+			console.log(error);
+		});
+	};
+
+	$scope.deleteCnf = function(article){
+		$scope.deleteArticle = article;
+	};
+
+	$scope.deleteEvent = function(deleteArticle){
+		$scope.articles.$remove(deleteArticle);
+		$("#deleteModal").modal('hide');
+	};
+
+	$scope.logout = function(){
+		CommonProp.logoutUser();
+	}
+}])
