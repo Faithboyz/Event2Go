dbd26cf5b55cec658ab8af6cd84e5e22f95be7a5
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-03-14 13:27:15 -0400

    edit profile v0.1

--- platforms/browser/www/account/account.js
+++ platforms/browser/www/account/account.js
@@ -12,19 +12,36 @@ angular.module('Event2Go.account', ['ngRoute', 'firebase'])
 .controller('AccountCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', '$location', '$timeout', function($scope, CommonProp, $firebaseArray, $firebaseObject, $location, $timeout){
 	$scope.username = CommonProp.getUser();
 	$scope.userAccount = {};
+	$scope.location = "Toronto, ON";
 	$scope.showLoad = true;
+	$scope.showData = false;
+	$scope.showEdit = false;
+	$scope.ID = null;
 	var ref = firebase.database().ref("Account");
 	ref.orderByChild("email").equalTo($scope.username).on("child_added", function(snapshot) {
 			$scope.userAccount = snapshot.val();
 			 $timeout(function(){ 
 			    $scope.userAccount = snapshot.val();
-			    $scope.showLoad = false;
-			    console.log(userAccount.name);
+			    $scope.showLoad = false;$scope.showData = true;$scope.showEdit = false;
+			    $scope.ID = snapshot.key;
 			  });
 		}, function(error) {
 			  // The Promise was rejected.
 			  console.error(error);});
 
+	$scope.editPost = function(id){
+		//var ref = firebase.database().ref().child('Articles/' + id);
+		$scope.editPostData = $firebaseObject(ref);
+	};
+	$scope.Edit = function(id){
+		$scope.showLoad = false;$scope.showData = false;$scope.showEdit = true;
+	};
+	$scope.Cancel = function(id){
+		$scope.showLoad = false;$scope.showData = true;$scope.showEdit = false;
+	};
+	$scope.Update = function(id){
+		$scope.showLoad = false;$scope.showData = true;$scope.showEdit = false;
+	};
 
 	if(!$scope.username){
 		$location.path('/home');
