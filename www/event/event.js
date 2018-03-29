2113be067197837e10b4a004151bf6016d37812b
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-03-29 02:26:12 -0400

    edit + delete v0.1

--- www/event/event.js
+++ www/event/event.js
@@ -10,12 +10,15 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 }])
 
 .controller('EventCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', '$location', '$timeout', function($scope, CommonProp, $firebaseArray, $firebaseObject, $location, $timeout){
-	$scope.user = { name: 'EventName',description: 'EventDescription',location: 'EventLocation', date: 1522206895 };
+	$scope.user = { name: 'Event Name',description: 'Event Description',location: 'Event Location', date: 1522206895 };
 	$scope.eventForm = {};
+	$scope.edit = { name: 'Event Name',description: 'Event Description',location: 'Event Location', date: 1522206895 };
+	$scope.editForm = {};
 	$scope.username = CommonProp.getUser();
 	$scope.userEvent = {};
 	$scope.showCreate = false;
-	$scope.showList = true;
+	$scope.showList = false;
+	$scope.showLoad = true;
 	$scope.current = Date.now();
 
 	var add = firebase.database().ref().child('Event');
@@ -31,14 +34,14 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 				var date = new Date(timestamp * 1000);
 				var datevalues = ('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
 				$scope.bdatetime = datevalues;*/
+				$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;
 			  });
 		}, function(error) {
 			  // The Promise was rejected.
 			  console.error(error);});
 
-
 	$scope.Create = function(){
-		$scope.event.$add({
+		$scope.events.$add({
 			event: $scope.user.name,
 			email: $scope.username,
 			description: $scope.user.description,
@@ -46,16 +49,34 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 			date: $scope.user.date.getTime()
 		}).then(function(add){
 			console.log("Event Created !!");
-			$scope.showCreate = false;$scope.showList = true;
+			$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;$scope.showEdit = false;
 		});
 	}
 
 	$scope.Createform = function(){
-		$scope.showCreate = true;$scope.showList = false;
+		$scope.showCreate = true;$scope.showList = false;$scope.showLoad = false;$scope.showEdit = false;
 	}
+	$scope.editEvent = function(id){
+		var edit = firebase.database().ref().child('Event/' + id);
+		$timeout(function(){
+		$scope.edits = $firebaseObject(edit);
+		$scope.showCreate = false;$scope.showList = false;$scope.showLoad = false;$scope.showEdit = true;	
+	});
 
+	}
+	$scope.deleteEvent = function(id){
+		$scope.showCreate = false;$scope.showList = false;$scope.showLoad = true;$scope.showEdit = false;
+		var ref = firebase.database().ref().child('Event/' + id);
+		$timeout(function(){
+			ref.remove();
+			console.log("Value deleted");
+			var reload = firebase.database().ref().child('Event');
+			$scope.events = $firebaseArray(reload);
+			$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;$scope.showEdit = false;
+		});
+	}
 	$scope.Cancel = function(){
-		$scope.showCreate = false;$scope.showList = true;
+		$scope.showCreate = false;$scope.showList = true;$scope.showEdit = false;$scope.showLoad = false;	
 	}
 
 	if(!$scope.username){
