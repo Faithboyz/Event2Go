90a4dca13e5b23a2c0b1eeeb121ea7a385d17355
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-03-30 02:41:23 -0400

    event edit v0.1

--- platforms/browser/www/event/event.js
+++ platforms/browser/www/event/event.js
@@ -10,15 +10,13 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 }])
 
 .controller('EventCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', '$location', '$timeout', function($scope, CommonProp, $firebaseArray, $firebaseObject, $location, $timeout){
-	$scope.user = { name: 'Event Name',description: 'Event Description',location: 'Event Location', date: 1522206895 };
+	$scope.user = { event: 'Event Name',description: 'Event Description',Location: 'Event Location', date: 1522206895 };
 	$scope.eventForm = {};
-	$scope.edit = { name: 'Event Name',description: 'Event Description',location: 'Event Location', date: 1522206895 };
+	$scope.edit = { event: 'Event Name',description: 'Event Description',Location: 'Event Location', date: 1522206895 };
 	$scope.editForm = {};
 	$scope.username = CommonProp.getUser();
 	$scope.userEvent = {};
-	$scope.showCreate = false;
-	$scope.showList = false;
-	$scope.showLoad = true;
+	$scope.showCreate = false;$scope.showList = false;$scope.showLoad = true;$scope.showEdit = false;
 	$scope.current = Date.now();
 
 	var add = firebase.database().ref().child('Event');
@@ -27,25 +25,25 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 
 	var ref = firebase.database().ref("Event");
 	ref.orderByChild("email").equalTo($scope.username).on("child_added", function(snapshot) {
-			 $timeout(function(){ 
-			    $scope.userEvent = snapshot.val();
-			    console.log($scope.userEvent);
-			    /*var timestamp = data[0].bdatetime;
+		 	$timeout(function(){ 
+		    	$scope.userEvent = snapshot.val();
+		    	console.log($scope.userEvent);
+		    	/*var timestamp = data[0].bdatetime;
 				var date = new Date(timestamp * 1000);
 				var datevalues = ('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
 				$scope.bdatetime = datevalues;*/
-				$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;
-			  });
+				$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;$scope.showEdit = false;
+		  	});
 		}, function(error) {
 			  // The Promise was rejected.
 			  console.error(error);});
 
 	$scope.Create = function(){
 		$scope.events.$add({
-			event: $scope.user.name,
+			event: $scope.user.event,
 			email: $scope.username,
 			description: $scope.user.description,
-			Location: $scope.user.location,
+			Location: $scope.user.Location,
 			date: $scope.user.date.getTime()
 		}).then(function(add){
 			console.log("Event Created !!");
@@ -53,16 +51,34 @@ angular.module('Event2Go.event', ['ngRoute', 'firebase'])
 		});
 	}
 
+	$scope.Update = function(id){
+		var ref = firebase.database().ref().child('Event/' + id);
+		if($scope.editPostData.event!=null && $scope.editPostData.date!=null && $scope.editPostData.description!=null && $scope.editPostData.Location!=null)
+		{
+			ref.update({
+				event: $scope.editPostData.event,
+				email: $scope.username,
+				description: $scope.editPostData.description,
+				Location: $scope.editPostData.Location,
+				date: $scope.editPostData.date.getTime()
+			}).then(function(ref){
+			}, function(error){
+				console.log(error);
+			});
+			$scope.showCreate = false;$scope.showList = true;$scope.showLoad = false;$scope.showEdit = false;
+		}
+		else{
+			console.log("Error with event creation!!");
+		}
+	}
+
 	$scope.Createform = function(){
 		$scope.showCreate = true;$scope.showList = false;$scope.showLoad = false;$scope.showEdit = false;
 	}
 	$scope.editEvent = function(id){
-		var edit = firebase.database().ref().child('Event/' + id);
-		$timeout(function(){
-		$scope.edits = $firebaseObject(edit);
+		var ref = firebase.database().ref().child('Event/' + id);
+		$scope.editPostData = $firebaseObject(ref);
 		$scope.showCreate = false;$scope.showList = false;$scope.showLoad = false;$scope.showEdit = true;	
-	});
-
 	}
 	$scope.deleteEvent = function(id){
 		$scope.showCreate = false;$scope.showList = false;$scope.showLoad = true;$scope.showEdit = false;
