85b2b023a8f777cbb150e77570d08ed4875c127b
Faithboyz <35408516+Faithboyz@users.noreply.github.com>
2018-04-16 19:55:59 -0400

    monotonic

---
+++ Real time/rate monotonic scheduler/Engine/InitDialog.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SimEngine
+{
+    partial class InitDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numProcesses = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SimulationTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EventsList = new System.Windows.Forms.ListView();
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Parameter1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Parameter2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.Waitinglist = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numProcesses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numProcesses
+            // 
+            this.numProcesses.Location = new System.Drawing.Point(31, 27);
+            this.numProcesses.Name = "numProcesses";
+            this.numProcesses.Size = new System.Drawing.Size(39, 20);
+            this.numProcesses.TabIndex = 0;
+            this.numProcesses.ValueChanged += new System.EventHandler(this.setNumberOfProcesses);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Processes";
+            // 
+            // SimulationTime
+            // 
+            this.SimulationTime.AutoSize = true;
+            this.SimulationTime.Location = new System.Drawing.Point(28, 91);
+            this.SimulationTime.Name = "SimulationTime";
+            this.SimulationTime.Size = new System.Drawing.Size(13, 13);
+            this.SimulationTime.TabIndex = 5;
+            this.SimulationTime.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Simulation Time (Milliseconds)";
+            // 
+            // EventsList
+            // 
+            this.EventsList.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.EventsList.BackColor = System.Drawing.SystemColors.Control;
+            this.EventsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Time,
+            this.pID,
+            this.Parameter1,
+            this.Parameter2});
+            this.EventsList.Location = new System.Drawing.Point(12, 148);
+            this.EventsList.Name = "EventsList";
+            this.EventsList.OwnerDraw = true;
+            this.EventsList.Size = new System.Drawing.Size(361, 414);
+            this.EventsList.TabIndex = 7;
+            this.EventsList.UseCompatibleStateImageBehavior = false;
+            this.EventsList.View = System.Windows.Forms.View.Details;
+            this.EventsList.DrawColumnHeader += new System.Windows.Forms.DrawListViewColumnHeaderEventHandler(this.EventsList_DrawColumnHeader);
+            this.EventsList.DrawItem += new System.Windows.Forms.DrawListViewItemEventHandler(this.EventsList_DrawItem);
+            // 
+            // Time
+            // 
+            this.Time.Text = "Sim-Time";
+            this.Time.Width = 90;
+            // 
+            // pID
+            // 
+            this.pID.Text = "Process ID";
+            this.pID.Width = 92;
+            // 
+            // Parameter1
+            // 
+            this.Parameter1.Text = "Priority";
+            this.Parameter1.Width = 89;
+            // 
+            // Parameter2
+            // 
+            this.Parameter2.Text = "Period";
+            this.Parameter2.Width = 76;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Running Processes";
+            // 
+            // Waitinglist
+            // 
+            this.Waitinglist.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.Waitinglist.BackColor = System.Drawing.SystemColors.Control;
+            this.Waitinglist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.Waitinglist.Location = new System.Drawing.Point(379, 148);
+            this.Waitinglist.Name = "Waitinglist";
+            this.Waitinglist.OwnerDraw = true;
+            this.Waitinglist.Size = new System.Drawing.Size(361, 414);
+            this.Waitinglist.TabIndex = 9;
+            this.Waitinglist.UseCompatibleStateImageBehavior = false;
+            this.Waitinglist.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sim-Time";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Process ID";
+            this.columnHeader2.Width = 92;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Priority";
+            this.columnHeader3.Width = 89;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Period";
+            this.columnHeader4.Width = 76;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(493, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Waiting Processes";
+            // 
+            // InitDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 574);
+            this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Waitinglist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EventsList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SimulationTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numProcesses);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "InitDialog";
+            this.Text = "Engine";
+            this.Load += new System.EventHandler(this.InitDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numProcesses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numProcesses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SimulationTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView EventsList;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.ColumnHeader Parameter1;
+        private System.Windows.Forms.ColumnHeader pID;
+        private System.Windows.Forms.ColumnHeader Parameter2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView Waitinglist;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Real time/rate monotonic scheduler/Engine/InitDialog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimEngine
+{
+    public partial class InitDialog : Form
+    {
+        public delegate void ParamsUpdateHandler(object sender, ParamsEventArgs e);
+        public event ParamsUpdateHandler OnUpdateParams;
+
+        private Params p = new Params();
+
+
+        public InitDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void InitDialog_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void setNumberOfProcesses(object sender, EventArgs e)
+        {
+            p.numProcesses = (int)numProcesses.Value;
+            OnUpdateParams(this, new ParamsEventArgs(p));
+        }
+
+
+        public void UpdateSimulationTime(int t)
+        {
+            SimulationTime.Text = t.ToString();
+        }
+
+        public void UpdateEventList(ListViewItem item)
+        {
+            EventsList.Items.Add(item);
+        }
+
+        private void EventsList_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.LawnGreen, e.Bounds);
+            e.DrawText();
+        }
+
+        private void EventsList_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+        
+    }
+
+    public class Params
+    {
+        public int numProcesses { get; set; }
+    }
+
+    public class ParamsEventArgs : EventArgs
+    {
+        public Params SimParams = new Params();
+
+        public ParamsEventArgs(Params p)
+        {
+            SimParams.numProcesses = p.numProcesses;
+        }
+    }
+}
